021e7ca54c2e027657fdc5d11d87f5d40db9c661
willy <w.yunnal@gmail.com>
2015-05-22 19:10:39 +0700

    setup new the_role version 3

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,9 @@
 class Role < ActiveRecord::Base
-  acts_as_role
+  include TheRole::Api::Role
   attr_accessible :name, :title, :description, :the_role 
 end
+
+# class Role < ActiveRecord::Base
+#   acts_as_role
+#   attr_accessible :name, :title, :description, :the_role 
+# end
--- config/initializers/the_role.rb
+++ config/initializers/the_role.rb
@@ -1,11 +1,19 @@
 # TheRole.config.param_name => value
 
 TheRole.configure do |config|
-  # config.layout                     = :application
+  # [ Devise => :authenticate_user! | Sorcery => :require_login ]
+  # config.login_required_method = :authenticate_user!
+
+  # layout for Management panel
+  # config.layout = :the_role_management_panel
 
   # config.default_user_role          = nil
   # config.first_user_should_be_admin = false
   # config.access_denied_method       = :access_denied
-  # config.login_required_method      = :authenticate_user!
-  # config.destroy_strategy           = :restrict_with_exception # can be nil
+
+  # Dependent of Rails::VERSION
+  #
+  # [ :destroy, :delete_all, :nullify, :restrict, :restrict_with_exception, :restrict_with_error ]
+  # config.destroy_strategy = nil
 end
+
