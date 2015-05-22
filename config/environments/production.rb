da61667d61095b33412280487a4ecc47e7898cdd
willy <w.yunnal@gmail.com>
2015-05-22 17:01:20 +0700

    dont skip asset precompilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Ticketie::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.precompile += ['application.js', 
             'application.css', 
               'bootstrap-email.css', 
